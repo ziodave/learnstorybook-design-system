fa3336173ffa75b39042744eb6984b2abfe25f6f
David Riccitelli <david@wordlift.io>
2020-04-08 14:23:40 +0200

    update.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,16 @@
 module.exports = {
-  stories: ["../src/**/*.stories.js"],
+  // automatically import all files ending in *.stories.js|mdx
+  stories: [
+    "../src/components/Intro.stories.mdx",
+    "../src/**/*.stories.(js|mdx)",
+  ],
   addons: [
     "@storybook/preset-create-react-app",
     "@storybook/addon-actions",
     "@storybook/addon-links",
     "@storybook/addon-storysource",
     "@storybook/addon-knobs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
   ],
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { addDecorator } from "@storybook/react";
+import { withA11y } from "@storybook/addon-a11y";
+
 import { GlobalStyle } from "../src/shared/global";
 
+addDecorator(withA11y);
 addDecorator((story) => (
   <>
     <GlobalStyle />
